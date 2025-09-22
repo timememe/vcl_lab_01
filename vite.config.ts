ed5211d58c8ee5554e8032176f65aa5f53d59417
timememe <thetimememe@gmail.com>
2025-09-22 18:10:40 +0500

    16

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     
     return {
+      plugins: [react()],
+      css: {
+        postcss: './postcss.config.js',
+      },
       server: {
         host: '0.0.0.0',
         port: parseInt(process.env.PORT || '5173'),
